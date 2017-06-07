c231f67c7edd74e2b8f6a38edf1ba3ff4c7968e3
Yohanan Assefa <nanahoy@gmail.com>
2017-06-07 07:41:37 -0600

    Integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,53 @@
 class PortfoliosController < ApplicationController
+  before_action :find_portfolio, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    if @portfolio.save
+      redirect_to @portfolio
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @portfolio.update_attributes(portfolio_params)
+      redirect_to @portfolio
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @portfolio.destroy
+
+    render 'index'
+  end
+
+  private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image,
+                                        :thumb_image)
+    end
+
+    def find_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
 end
