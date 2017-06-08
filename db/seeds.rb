709bbbc254fc1876193bba76bfa75929a29e5cb0
Yohanan Assefa <nanahoy@gmail.com>
2017-06-08 13:30:44 -0400

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,4 +65,13 @@ end
   thumb_image: "http://placehold.it/350x200"
   )
 end
+
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
