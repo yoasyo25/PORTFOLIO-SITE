eee391b81b0c035f93f52845910f8979a613e71e
Yohanan Assefa <nanahoy@gmail.com>
2017-06-09 13:18:10 -0400

    Added nav bar and default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+  extend ActiveSupport:: Concern
+
+  included do
+    before_action :set_page_defaults
+  end
+
+  def set_page_defaults
+    @page_title  = "Devcamp Portfolio | My Portfolio Website"
+    @seo_keyword = "Yohanan Assefa's Portfolio"
+  end
+end
