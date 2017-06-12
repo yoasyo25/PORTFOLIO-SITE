0c7fbc088a8f7622f357276c69588cfd133897ce
Yohanan Assefa <nanahoy@gmail.com>
2017-06-12 15:05:47 -0400

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update,
+                                               :edit]}, site_admin: :all
   layout "blog"
 
   # GET /blogs
---
+++ app/models/guest_user.rb
@@ -0,0 +1,3 @@
+class GuestUser < User
+  attr_accessor :name, :last_name, :first_name, :email
+end
