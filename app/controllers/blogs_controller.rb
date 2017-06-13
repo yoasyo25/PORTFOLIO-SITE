d40a5d7b7a5dbabbb6f68bc620934b0b04704476
Yohanan Assefa <nanahoy@gmail.com>
2017-06-13 14:56:24 -0400

    Implemented partials for blogs, font awesome, pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.special_blogs
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = "My Portfolio Blog"
   end
 
@@ -69,7 +69,7 @@ class BlogsController < ApplicationController
     elsif @blog.published?
       @blog.draft!
     end
-        
+
     redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
