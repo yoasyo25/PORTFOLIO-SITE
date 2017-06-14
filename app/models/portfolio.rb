b00f80286e29ed7a8442cc7ee51a0b50f3c9450b
Yohanan Assefa <nanahoy@gmail.com>
2017-06-14 16:41:46 -0400

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,6 +5,9 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+
   def self.ruby
     where(subtitle: "Ruby")
   end
