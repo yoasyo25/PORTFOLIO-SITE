73ad4324032098b2775063e8a011a94fd4b45fc8
Yohanan Assefa <nanahoy@gmail.com>
2017-06-15 15:14:29 -0400

    Implemented twitter call on tech news page

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
                                              sign_up:  'register'}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
-  end 
+  end
   get 'ruby-items', to: 'portfolios#ruby'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 
   get 'about-me',   to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
