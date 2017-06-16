b1365b53fc41ca989d135741d50d8f0be99d12aa
Yohanan Assefa <nanahoy@gmail.com>
2017-06-16 16:14:46 -0400

    Added topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+resources :topics, only: [:index, :show]
+
   devise_for :users, path: '', path_names: { sign_in:  'login',
                                              sign_out: 'logout',
                                              sign_up:  'register'}
@@ -18,7 +20,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  
+
   mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
