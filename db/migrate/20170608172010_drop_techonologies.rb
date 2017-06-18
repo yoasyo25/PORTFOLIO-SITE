9206cbed3a5760006f4e1c4ebf17fe9bd0adb71d
Yohanan Assefa <nanahoy@gmail.com>
2017-06-18 15:08:27 -0400

    Commented out drop technologies table

--- db/migrate/20170608172010_drop_techonologies.rb
+++ db/migrate/20170608172010_drop_techonologies.rb
@@ -1,5 +1,5 @@
-class DropTechonologies < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :techonologies
-  end
-end
+# class DropTechonologies < ActiveRecord::Migration[5.0]
+#   def change
+#     drop_table :techonologies
+#   end
+# end
